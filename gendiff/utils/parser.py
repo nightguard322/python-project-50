5714bf79f8a2df4e05a89ea4afdbaf8f5b6a2077
sasha <sander_@inbox.ru>
2025-05-04 17:19:54 +0300

    fixes with imports

--- gendiff/utils/parser.py
+++ gendiff/utils/parser.py
@@ -15,28 +15,3 @@ def parse_file(filename: str) -> None:
     return open_file(filepath)
 
 
-def open_file(file_path) -> None:
-    """
-    Load and parse JSON and YAML/YML file
-
-    Args:
-        file_path: Path to the file (str or Path obj)
-
-    Return:
-        Parsed data as dict
-
-    Raises:
-        ValueError: file extension not supported
-        FileNotFoundError: file doesn't exists
-        yaml.YAMLError: YAML reading error
-        json.JSONDecodeError: JSON parsing error
-    """
-    with file_path.open() as file:
-        match (file_path.suffix.lower()):
-            case '.json':
-                return json.load(file)
-            case '.yaml' | '.yml':
-                raise ValueError('opening yaml')
-
-            case _:
-                raise ValueError(f"Unknown file extension - {file_path.suffix}")
